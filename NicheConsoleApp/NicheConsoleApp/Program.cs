c67611d59a5942101339e5bde2b43502e9dfeccd
lynnsamuelson <lynn.samuelson@gmail.com>
2016-05-23 16:28:23 -0500

    added console test

---
+++ NicheConsoleApp/NicheConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NicheConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("** 5/23/16 NichProvider Console App Test **");
+            Console.ReadKey();
+        }
+    }
+}
